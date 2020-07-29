9fd5cc59a4dc787474c5b1713b78411eab4869f9
Jonas Zaugg <jonas.zaugg@yahoo.com>
2020-07-29 15:30:00 +0200

    Add more stuff in xcframework

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 
 let version = "2.0.0"
 let checksum = "ced6b1d5691f948df49206eba9abe75fcabbef3dcfae685e8352c8df5d60ea22"
+let binChecksum = "7899e6d3baa52a8061c865cffd136afac2f73a00aa5155ce861307ce630ee617"
 let url = "https://github.com/sparkle-project/Sparkle/releases/download/\(version)/Sparkle-SPM-\(version).zip"
 
 let package = Package(
