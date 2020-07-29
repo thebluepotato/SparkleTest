cd51349c64cf12e9d84bd1a3649554e3a0a36265
Jonas Zaugg <jonas.zaugg@yahoo.com>
2020-07-29 23:48:40 +0200

    Add more files in zip next to framework

--- Package.swift
+++ Package.swift
@@ -2,9 +2,9 @@
 import PackageDescription
 
 let version = "2.0.0"
-let checksum = "ced6b1d5691f948df49206eba9abe75fcabbef3dcfae685e8352c8df5d60ea22"
-let binChecksum = "7899e6d3baa52a8061c865cffd136afac2f73a00aa5155ce861307ce630ee617"
-let url = "https://github.com/sparkle-project/Sparkle/releases/download/\(version)/Sparkle-SPM-\(version).zip"
+let checksum = "fa0deb67f6d47c8020bd795fa98d6b495eecc7c7959539e5dc49505fd5f4bc5f"
+//let url = "https://github.com/sparkle-project/Sparkle/releases/download/\(version)/Sparkle-SPM-\(version).zip"
+let url = ""
 
 let package = Package(
     name: "Sparkle",
@@ -15,14 +15,10 @@ let package = Package(
             targets: ["Sparkle"])
     ],
     targets: [
-        //        .binaryTarget(
-        //            name: "Sparkle",
-        //            url: url,
-        //            checksum: checksum
-        //        )
         .binaryTarget(
             name: "Sparkle",
-            path: "./Sparkle.xcframework"
+            url: url,
+            checksum: checksum
         )
     ]
 )
