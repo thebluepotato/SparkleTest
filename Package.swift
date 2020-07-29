c19d0ddf46a73734159bdd84c913390d9fecda1a
Jonas Zaugg <jonas.zaugg@yahoo.com>
2020-07-29 23:50:49 +0200

    Add correct url

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let version = "2.0.0"
+let version = "1.24.0"
 let checksum = "fa0deb67f6d47c8020bd795fa98d6b495eecc7c7959539e5dc49505fd5f4bc5f"
 //let url = "https://github.com/sparkle-project/Sparkle/releases/download/\(version)/Sparkle-SPM-\(version).zip"
-let url = ""
+let url = "https://github.com/thebluepotato/SparkleTest/raw/master/Sparkle-\(version)-test.zip"
 
 let package = Package(
     name: "Sparkle",
